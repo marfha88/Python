bf756cad0ff617f939dd5fa9f0860ee6812ab8a9
marfha88 <77161991+marfha88@users.noreply.github.com>
2021-01-08 19:48:34 +0100

    Add files via upload

---
+++ Variabels.py
@@ -0,0 +1,9 @@
+character_name = "Alex"
+character_age = "35"
+is_male = True
+print("guys name is " + character_name)
+print("He is " + character_age + " years old")
+
+character_name = "Tom"
+print("he loves the name " + character_name)
+print("and hes age that is " + character_age)
