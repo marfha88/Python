0891a0a633e917fb24bc15dd191026fdce6331dd
marfha88 <77161991+marfha88@users.noreply.github.com>
2021-01-12 16:16:33 +0100

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,9 @@
+
+color = input("Enter a color ")
+plural = input("Enter thing ")
+someone = input("Enter a name ")
+
+print("Roses are " + color)
+print(plural + " are blue")
+print("I love " + someone)
+
